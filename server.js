f5f5960cc8f79cb3f6b9a7f71cb89d91df63cad4
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-04 15:05:00 -0800

    Adding basic scaffolding for MVC.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+
+require('dotenv').config();
+
+const usersRouter = require('./routes/api/users');
+// const ticketsRouter = require('./routes/api/tickets');
+
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/users', usersRouter);
+app.use('/api/weapons', weaponRouter);
+
+const port = process.env.PORT || 3001;
+	
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
